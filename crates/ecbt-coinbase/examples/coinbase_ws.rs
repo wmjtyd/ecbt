cbbebc81a79eea17aeecbb8eb718a514c21453de
immno <dancermno@gmail.com>
2022-05-09 15:06:29 +0800

    [examples]: complete the example of http and ws

--- crates/ecbt-coinbase/examples/coinbase_ws.rs
+++ crates/ecbt-coinbase/examples/coinbase_ws.rs
@@ -2,8 +2,6 @@ use std::time::Duration;
 
 use ecbt_coinbase::model::websocket::{CoinbaseSubscription, CoinbaseWebsocketMessage};
 use ecbt_coinbase::{CoinbaseParameters, CoinbaseWebsocket};
-use ecbt_exchange::model::currency::Currency;
-use ecbt_exchange::model::market_pair::MarketPair;
 use ecbt_exchange::model::websocket::WebSocketResponse;
 use ecbt_exchange::shared::Result;
 use ecbt_exchange::stream::ExchangeStream;
@@ -11,12 +9,12 @@ use ecbt_exchange::stream::ExchangeStream;
 #[tokio::main]
 async fn main() -> Result<()> {
     let binance_ws = CoinbaseWebsocket::new(CoinbaseParameters::sandbox()).await?;
-    let symbol = MarketPair(Currency::BTC, Currency::ETH).to_string();
+    let symbol = "ETH-BTC".to_string();
     binance_ws
         .subscribe(CoinbaseSubscription::Matches(symbol), call_back)
         .await?;
 
-    std::thread::sleep(Duration::from_secs(10));
+    std::thread::sleep(Duration::from_secs(100));
     Ok(())
 }
 
